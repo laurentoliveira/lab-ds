4b9ad79c64f6dae81a03a8eba31c5080e49ce8a2
laurentoliveira <laurenvilela96@gmail.com>
2022-10-11 22:29:22 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ullam
+      quibusdam adipisci expedita quas mollitia tenetur, explicabo dolorum eos
+      nobis sit nulla quam amet iste illum minima nihil voluptatibus.
+      Asperiores!
+    </h1>
+  )
+}
+
+export default App
